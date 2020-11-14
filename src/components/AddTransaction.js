6e58de66a3ecfc9c8c660f32e289ebf9f79f53e1
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-14 19:05:02 +0500

    Update AddTransaction.js
    made a state for description and transaction amount

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const AddTransaction = ()=> {
+
+    let [description, setDescription] = useState();
+    let [transactionAmount, setTransactionAmount] = useState();
+
     return(
         <div>
             <h3>Add New Transaction</h3>
@@ -12,6 +16,8 @@ export const AddTransaction = ()=> {
                     <input type="text" 
                            id="description" 
                            placeholder="Detail of Trnsaction"
+                           value={description}
+                           onChange={(e)=> setDescription(e.target.value)}
                     />
                 </div>
                 <div className="form-control">
@@ -21,6 +27,8 @@ export const AddTransaction = ()=> {
                     <input  type="number"
                             id="transactionamount"
                             placeholder="Enter Transaction amount"
+                            value={transactionAmount}
+                            onChange={(e)=> setTransactionAmount(e.target.value)}
                     />
                 </div>
                 <button className="btn">
