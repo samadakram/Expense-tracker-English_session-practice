8743a8fb09279a9a2f84823eaaaabe1611310dde
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-14 18:00:09 +0500

    Create AccountSummary.js

---
+++ src/components/AccountSummary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const AccountSummary = ()=> {
+    return(
+        <div className="inc-exp-container">
+            <div>
+                <h4>Income</h4>
+            </div>
+            <div>
+                <h4>Expense</h4>
+            </div>
+        </div>
+    );
+}
