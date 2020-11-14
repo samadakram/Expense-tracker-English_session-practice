044333ef6cd76cd96d79fc369b5365251dfbcbe9
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-14 18:03:44 +0500

    Update AccountSummary.js

--- src/components/AccountSummary.js
+++ src/components/AccountSummary.js
@@ -5,9 +5,15 @@ export const AccountSummary = ()=> {
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
+                <p>
+                    $0.00
+                </p>
             </div>
             <div>
                 <h4>Expense</h4>
+                <p>
+                    $0.00
+                </p>
             </div>
         </div>
     );
