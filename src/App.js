b2a49a293b42dc2eecb4d6703338fe628ba26395
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-14 17:40:24 +0500

    Update App.js
    imported Header.js in App.js and component called in div

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+// import components
+import { Header } from './components/Header'
+
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Header />
+    </div>
   );
 }
 
