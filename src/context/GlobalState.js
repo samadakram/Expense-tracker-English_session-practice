f7b769a3946c4040ac73da0ce767952b6d346ad3
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-15 01:01:08 +0500

    Update GlobalState.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,6 +2,9 @@ import React, { createContext, useReducer } from 'react';
 
 const initialState = {
     transactions: [
-        {}
+        {id: 1, descrition: "Income 1", transactionAmount: 1000},
+        {id: 2, descrition: "Expense 2", transactionAmount: -100},
+        {id: 3, descrition: "Income 2", transactionAmount: 1000},
+        {id: 4, descrition: "Expense 1", transactionAmount: -500}
     ]
 }
