90c666141b09926fc6f5ce2cc4a9f2fc397b4af6
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-15 01:34:08 +0500

    Update TransactionHistory.js

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -2,6 +2,9 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionHistory = ()=> {
+
+    const { transactions } = useContext(GlobalContext);
+
     return(
         <div>
             <h3>Transaction History</h3>
