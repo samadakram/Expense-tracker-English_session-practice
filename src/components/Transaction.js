3c6480724b8fb51f7851a2ecee431d2de04a42cb
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-16 17:41:43 +0500

    Update Transaction.js
    added list item in transaction.js

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export const Transaction = () => {
     return(
-        <div></div>
+        <div>
+            <li className="plus">
+                Project 1 Income
+                <span>%1,000</span>
+                <button className="delete-btn">X</button>
+            </li>
+        </div>
     )
 }
