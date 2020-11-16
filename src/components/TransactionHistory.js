11755aeb0f130a20372d56ddb10c40b8236f3519
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-16 17:42:26 +0500

    Update TransactionHistory.js
    removed map function from ul

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -11,10 +11,7 @@ export const TransactionHistory = () => {
         <div>
             <h3>Transaction History</h3>
             <ul className="list">
-                {transactions.map(transaction => (
-                    <Transaction key={transaction.id} transaction={transaction} />
-                )
-                )}
+                
             </ul>
         </div>
     );
