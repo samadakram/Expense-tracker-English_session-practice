5b9d8063e9c64bb1cd7ace398f617b851e499ce1
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-16 18:43:43 +0500

    Update GlobalState.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -5,10 +5,10 @@ import AppReducer from './AppReducer';
 
 const initialState = {
     transactions: [
-        {id: 1, descrition: "Income 1", transactionAmount: 1000},
-        {id: 2, descrition: "Expense 2", transactionAmount: -100},
-        {id: 3, descrition: "Income 2", transactionAmount: 1000},
-        {id: 4, descrition: "Expense 1", transactionAmount: -500}
+        {id: 1, description: "Income 1", transactionAmount: 1000},
+        {id: 2, description: "Expense 2", transactionAmount: -100},
+        {id: 3, description: "Income 2", transactionAmount: 1000},
+        {id: 4, description: "Expense 1", transactionAmount: -500}
     ]
 }
 
