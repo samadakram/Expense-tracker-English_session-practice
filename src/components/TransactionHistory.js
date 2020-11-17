0981867fa9f46fd3964b4d2152c69d4f4f01f55b
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-17 20:14:36 +0500

    Update TransactionHistory.js
    removed li

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -10,11 +10,7 @@ export const TransactionHistory = () => {
         <div>
             <h3>Transaction History</h3>
             <ul className="list">
-                {transactions.map(transaction => (<li className="plus">
-                    {transaction.text}
-                <span> ${transaction.amount} </span>
-                    <button className="delete-btn">X</button>
-                </li>))}
+                {transactions.map(transaction => ())}
             </ul>
         </div>
     );
