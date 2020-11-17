fecc69c5a049fce92b6daa813dfc17bf9d522309
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-17 18:55:12 +0500

    Update TransactionHistory.js
    removed functionality from TransactionHistory.js

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -1,20 +1,13 @@
-import React, { useContext } from 'react';
-import { GlobalContext } from '../context/GlobalState';
+import React from 'react';
 
-import { Transaction } from './Transaction';
 
 export const TransactionHistory = () => {
 
-    const { transactions } = useContext(GlobalContext);
 
     return (
         <div>
             <h3>Transaction History</h3>
-            <ul className="list">
-                {transactions.map(transaction => (
-                    <Transaction key={transaction.id} transaction={transaction} />
-                )
-                )}
+            <ul>
             </ul>
         </div>
     );
