0deae038bc0504ac877938d9d08db9975e720833
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-18 02:59:16 +0500

    Update GlobalState.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -25,9 +25,17 @@ export const GlobalProvider = ({children})=> {
         });
     }
 
+    function addTransaction(transaction){
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
+
     return(<GlobalContext.Provider value={{
         transactions: state.transactions,
-        deleteTransaction
+        deleteTransaction,
+        addTransaction
         
     }}>
         {children}
