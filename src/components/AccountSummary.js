3b314102897651d4872175ff6cf3e717039651aa
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-18 02:35:04 +0500

    Update AccountSummary.js

--- src/components/AccountSummary.js
+++ src/components/AccountSummary.js
@@ -5,6 +5,13 @@ export const AccountSummary = ()=> {
 
     const { transactions } = useContext(GlobalContext);
 
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const income = amounts
+    .filter(item  => item > 0)
+    .reduce((acc, item)=> (acc += item), 0)
+    .toFixed(2);
+
     return(
         <div className="inc-exp-container">
             <div>
