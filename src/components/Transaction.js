0c65f57b69f4b597553d39e40d94c7313f8872d4
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-17 18:52:18 +0500

    Update Transaction.js
    removed functionality from Transaction.js

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-export const Transaction = ({transaction}) => {
+export const Transaction = () => {
     return(
         <div>
-            <li className="plus">
-                {transaction.description}
-                <span> {transaction.transactionAmount} </span>
-                <button className="delete-btn">X</button>
-            </li>
+        
         </div>
     )
 }
