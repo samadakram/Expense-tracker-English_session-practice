b85f41f3cf5177d9eb965865c939b92180da90c1
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-18 03:15:15 +0500

    Update AddTransaction.js

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -14,7 +14,7 @@ export const AddTransaction = ()=> {
         const newTransaction = {
             id: Math.floor(Math.random() * 100000000),
             text,
-            amount
+            amount: +amount
         }
 
         addTransaction(newTransaction);
