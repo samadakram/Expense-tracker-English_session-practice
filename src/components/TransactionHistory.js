77bb836f4fdc4989cee18d0fe43e5f4ac013a4f3
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-17 18:58:55 +0500

    Update TransactionHistory.js

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -7,7 +7,17 @@ export const TransactionHistory = () => {
     return (
         <div>
             <h3>Transaction History</h3>
-            <ul>
+            <ul className="list">
+            <li className="plus">
+                Project 1 Income
+                <span> $1,000 </span>
+                <button className="delete-btn">X</button>
+            </li>
+            <li className="minus">
+                Project 1 Salaries
+                <span> $500 </span>
+                <button className="delete-btn">X</button>
+            </li>
             </ul>
         </div>
     );
