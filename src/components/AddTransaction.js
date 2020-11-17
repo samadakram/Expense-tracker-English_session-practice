446e2c414eac22ba0ff75b39020f1862e92cc0ed
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-17 19:17:47 +0500

    Update AddTransaction.js

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const AddTransaction = ()=> {
 
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
 
     return(
         <div>
@@ -14,6 +16,8 @@ export const AddTransaction = ()=> {
                     <input type="text" 
                            id="description" 
                            placeholder="Detail of Trnsaction"
+                           value={text}
+                           onChange={(e)=> setText(e.target.value)}
                     />
                 </div>
                 <div className="form-control">
@@ -23,6 +27,8 @@ export const AddTransaction = ()=> {
                     <input  type="number"
                             id="transactionamount"
                             placeholder="Enter Transaction amount"
+                            value={amount}
+                            onChange={(e)=> setAmount(e.target.value)}
                     />
                 </div>
                 <button className="btn">
