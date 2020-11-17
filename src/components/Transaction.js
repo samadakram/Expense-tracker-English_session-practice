35825423ab7112d8c1dd6cd686a27880daa9df1a
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-17 20:28:03 +0500

    Update Transaction.js

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -5,7 +5,7 @@ export const Transaction = ({transaction}) => {
     const sign = transaction.amount < 0 ? '-' : '+';
 
     return (
-        <li className="plus">
+        <li className={transaction.amount < 0 ? "minus" : "plus"}>
             {transaction.text}
             <span> {sign} ${Math.abs(transaction.amount)} </span>
             <button className="delete-btn">X</button>
